5e9d37724ce576fcc81d090103154579e0944d81
Anthony Simmmons <simmonsar@gmail.com>
2017-05-25 19:00:48 -0400

    Completed join table validations set up cors

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+    has_many :joins
+    has_many :users, through: :joins
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :joins
+    has_many :events, through: :joins
 end
